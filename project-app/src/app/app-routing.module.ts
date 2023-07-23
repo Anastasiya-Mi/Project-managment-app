d84e6a37d78b1df31390446713a27a4b055235ca
“Anastasiya <anm6468276@gmail.com>
2023-07-23 21:38:45 +0300

    feat:add empty components, add login page and validation for login page

---
+++ project-app/src/app/app-routing.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
+import { StartPageComponent } from './components/start-page/start-page.component';
+
+const routes: Routes = [
+  {path:'',component:StartPageComponent},
+  {path:'login',component:LoginComponent},
+  {path:'**',component:NotFoundComponent},
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ project-app/src/app/components/login/login.component.ts
@@ -0,0 +1,23 @@
+import { Component,OnInit } from '@angular/core';
+import { FormGroup,FormControl,Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+
+export class LoginComponent {
+  loginForm!: FormGroup
+
+  ngOnInit():void {
+    this.loginForm = new FormGroup({
+      'email': new FormControl('',[Validators.required,Validators.email]),
+      'password': new FormControl('',[Validators.required,Validators.pattern(/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/)]),
+    })
+  }
+
+  submitLogin(){
+
+  }
+}
